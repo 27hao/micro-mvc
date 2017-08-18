11c64d500cca30262abf136e49d698ff0f688bb6
27hao <1119284545@qq.com>
2017-08-18 21:47:10 +0800

    项目初始化2

--- src/main/java/cn/bclearn/MicroFilter.java
+++ src/main/java/cn/bclearn/MicroFilter.java
@@ -1,3 +1,18 @@
 package cn.bclearn;
-public class MicroFilter implements{
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MicroFilter implements Filter{
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
 }
